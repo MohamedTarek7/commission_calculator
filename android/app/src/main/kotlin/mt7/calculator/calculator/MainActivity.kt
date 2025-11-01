c9d9365787bef437c148d8a665a9d43e39922775
Mohamed Tarek <mt444458@gmail.com>
2025-11-02 01:26:14 +0300

    New option: change currency within the app

--- android/app/src/main/kotlin/mt7/calculator/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/mt7/calculator/calculator/MainActivity.kt
@@ -1,5 +1,15 @@
 package mt7.calculator.calculator
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= 29) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+}
